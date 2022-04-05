0c34a3ffb1bedfe26747ddae2ac04eef08132ba3
Aditya <akaditya394@gmail.com>
2022-04-05 14:41:40 +0530

    call to action added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import styles from "./Home.module.css";
 import FAQ from "../components/FAQ";
 import NavbarNL from "../components/NavbarNL";
 import Footer from "../components/Footer";
+import Button from "../components/Button";
 
 function Home() {
   return (
@@ -11,7 +12,25 @@ function Home() {
         <div className={styles.nav}>
           <NavbarNL />
         </div>
-        <div className={styles.cta}></div>
+        <div className={styles.cta}>
+          <div>
+            <h1>
+              Unlimited movies, TV
+            </h1>
+            <h1>
+              shows and more.
+            </h1>
+          </div>
+          <div>
+            <h3>Watch anywhere. Cancel anytime.</h3>
+          </div>
+          <div>
+            <h5>Ready to watch? Sign In to continue!</h5>
+          </div>
+          <div>
+            <Button>Sign In</Button>
+          </div>
+        </div>
       </div>
       <div className={styles.lower}>
         <div className={styles.faqtext}>
