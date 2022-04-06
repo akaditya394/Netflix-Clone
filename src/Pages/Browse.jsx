8c30dba569cacfff96eaf0e2c89b1ded2c753124
Aditya <akaditya394@gmail.com>
2022-04-06 18:44:53 +0530

    job done

--- src/Pages/Browse.jsx
+++ src/Pages/Browse.jsx
@@ -3,6 +3,7 @@ import styles from "./Browse.module.css";
 import Navbar from "../components/Navbar";
 import Suggestions from "../components/Suggestions";
 import PeakyBlinders from "../assests/PeakyBlinders5.mp4";
+import PBL from "../assests/PeakyBlindersLogo.png";
 import PBPoster from "../assests/PBPoster.jpg";
 import Footer from "../components/Footer";
 
@@ -16,11 +17,14 @@ function Browse() {
           <source src={PeakyBlinders} type="video/ogg" />
           Your browser does not support the video tag.
         </video>
+        <div className={styles.pbl}>
+          <img src={PBL} alt="pbl" />
+        </div>
       </div>
-      <Suggestions />
-      <Suggestions />
-      <Suggestions />
-      <Footer className={styles.footer}/>
+      <Suggestions heading="Trending" />
+      <Suggestions heading="US TV Comedies about Friendship" />
+      <Suggestions heading="Only on Netflix" />
+      <Footer className={styles.footer} />
     </div>
   );
 }
--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -8,19 +8,19 @@ import MovieD from "../assests/movied.jpg";
 import MovieE from "../assests/moviee.webp";
 import MovieF from "../assests/movief.webp";
 
-function Suggestions() {
+function Suggestions(props) {
   return (
     <div className={styles.main}>
       <div>
-        <b>Trending</b>
+        <b>{props.heading}</b>
       </div>
       <div className={styles.movies}>
-        <MovieCard src={MovieA} />
-        <MovieCard src={MovieB} />
-        <MovieCard src={MovieC} />
-        <MovieCard src={MovieD} />
-        <MovieCard src={MovieE} />
-        <MovieCard src={MovieF} />
+        <MovieCard src={MovieA} name="Friends" />
+        <MovieCard src={MovieB} name="The Office"/>
+        <MovieCard src={MovieC} name="Community"/>
+        <MovieCard src={MovieD} name="Space Force"/>
+        <MovieCard src={MovieE} name="Super Store"/>
+        <MovieCard src={MovieF} name="Good Place"/>
       </div>
     </div>
   );
